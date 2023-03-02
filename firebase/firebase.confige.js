67976ffbf507bd23f8efd26c222a92ba98b6b36e
LV NT <levnhattan@gmail.com>
2023-03-02 15:55:16 +0700

    set up project

---
+++ firebase/firebase.confige.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtIuRFRFmO33wchDinRVGpqIw2ufsV4ic",
+  authDomain: "graduation-project-fceb8.firebaseapp.com",
+  projectId: "graduation-project-fceb8",
+  storageBucket: "graduation-project-fceb8.appspot.com",
+  messagingSenderId: "713369096421",
+  appId: "1:713369096421:web:3137bf9ee13d7d5478040c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
